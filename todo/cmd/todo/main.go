1df63ca8a7589247b61381ff3ce57c86e531ed31
acikgozb <berkacikgoz@armut.com>
2023-12-27 10:32:21 +0300

    [todo] Modify the default --help flag
    flag.Usage is a function which we can override to show custom message to users when they want to see the available flags.

--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -14,6 +14,14 @@ func main() {
 	listFlag := flag.Bool("list", false, "Lists all todo items that are not completed")
 	completeFlag := flag.Int("complete", 0, "Marks given itemNumber as completed")
 
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Todo tool. Developed by acikgozb.\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "The CLI is NOT production ready, keep this in mind while using.\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2023\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage information:\n")
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
 	l := &todo.List{}
