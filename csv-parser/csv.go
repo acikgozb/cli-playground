fd760b0b403eb1aa457343f2350095cfb5af1e14
acikgozb <berkacikgoz@armut.com>
2024-02-17 23:14:39 +0300

    Replace ReadAll with Read to lower the CPU and memory usage
    After profiling, it can be seen that the GC is simply doing too much work.
    This happens because we're allocating the whole data into one slice.
    
    To fix this, csv files are read one record at a time and csvReader.ReuseRecord is used to not allocate new slices for each record.
    
    These conclusions are made after profiling the CPU and mem usage of the CLI by adding a simple benchmark test which tests the CLI based on the csv under /benchmark directory.
    The pprof files are diagnosed by using go tool.

--- csv-parser/csv.go
+++ csv-parser/csv.go
@@ -27,36 +27,41 @@ type statsFunc func(data []float64) float64
 func csv2float(r io.Reader, column int) ([]float64, error) {
 	// Create the CSV reader used to read in data from CSV files
 	csvReader := csv.NewReader(r)
+	csvReader.ReuseRecord = true
 
 	// Adjusting for 0 based index
 	column--
 
-	// Read in all CSV data
-	allData, err := csvReader.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("cannot read data from file: %w", err)
-	}
-
-	// Convert string to float64 to make calculations on it
 	var data []float64
 
-	for i, row := range allData {
+	// As we can see from the benchmarks, it is not healthy to store all the data into a slice.
+	// Therefore, change the ReadAll() call to read record by record
+	for i := 0; ; i++ {
+		row, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
+
+		// Skip first row (which includes column names)
 		if i == 0 {
 			continue
 		}
 
-		// Checking number of columns in CSV file
 		if len(row) <= column {
-			return nil, fmt.Errorf("%w: File has only %d columns", ErrInvalidColumn, len(row))
+			return nil, fmt.Errorf("%w: file has only %d columns", ErrInvalidColumn, len(row))
 		}
 
-		// Try to convert data read into a float number
-		float, err := strconv.ParseFloat(row[column], 64)
+		// Convert string to float64 to make calculations on it
+		convertedValue, err := strconv.ParseFloat(row[column], 64)
 		if err != nil {
-			return nil, fmt.Errorf("%w: %s", ErrNotNumber, err)
+			return nil, fmt.Errorf("%w:%s", ErrNotNumber, err)
 		}
 
-		data = append(data, float)
+		data = append(data, convertedValue)
 	}
 
 	return data, nil
